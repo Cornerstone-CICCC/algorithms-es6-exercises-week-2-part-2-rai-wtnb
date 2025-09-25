11d30153b3a6ee2cc178643034dd71f4a0ad5e6e
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-24 20:27:21 -0700

    chore: complete ex02.js

--- ex02.js
+++ ex02.js
@@ -1,3 +1,5 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
-
+const nums = [0, 1, 2, 30, 4, 50];
+const dst = nums.find((num) => num > 10);
+console.log(dst);
