40fecbc0290dec10a8a5cbada5865620c302756b
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-24 20:27:12 -0700

    chore: complete ex01.js

--- ex01.js
+++ ex01.js
@@ -1,2 +1,5 @@
 // Sort Exercise
 // Instructions: Create an array of at least five numbers in random order. Write the code to sort this array in ascending order.
+const nums = [4, 2, 1, 0, 3];
+nums.sort();
+console.log(nums);
