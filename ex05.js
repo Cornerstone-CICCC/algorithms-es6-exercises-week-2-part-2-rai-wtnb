e81372478db7753a706c02720180694668159b3b
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-24 20:30:31 -0700

    chore: complete ex05.js

--- ex05.js
+++ ex05.js
@@ -1,3 +1,5 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
-
+const nums = [0, 1, 2, 3, 4];
+const isEvenIncluded = nums.some((num) => num % 2 === 0);
+console.log(isEvenIncluded);
