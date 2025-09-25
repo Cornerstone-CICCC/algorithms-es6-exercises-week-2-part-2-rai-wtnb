1fbe02d99a68fd93291d25a82df937aab617d69c
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-24 20:33:00 -0700

    chore: complete ex08.js

--- ex08.js
+++ ex08.js
@@ -1,3 +1,5 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
-
+const words = ["smartphone", "laptop", "desktop", "tablet", "printer"];
+const joined = words.join(" ");
+console.log(joined);
