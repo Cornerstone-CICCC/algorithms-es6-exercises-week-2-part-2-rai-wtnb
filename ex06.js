7433a9ad39df722658c37becbda091b1b4b81a64
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-24 20:31:11 -0700

    chore: complete ex06.js

--- ex06.js
+++ ex06.js
@@ -1,3 +1,5 @@
 // Every Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if every number in the array is positive.
-
+const nums = [0, 1, 2, 3, 4];
+const isAllPositive = nums.every((num) => num >= 0);
+console.log(isAllPositive);
